7e1a8c16191012ea0a0f430595b0fdaa947500ff
Piter <piotr.podyma10@gmail.com>
2019-06-18 22:29:36 +0200

    Basic files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import TodoList from "./TodoList"
+import store from "./Store/TodoStore"
+
 ReactDOM.render(<App />, document.getElementById('root'));
